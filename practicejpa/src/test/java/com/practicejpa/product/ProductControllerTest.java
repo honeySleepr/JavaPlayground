9cf5696f25cc6e23627eba7ae307b501a85919e4
honeySleepr <gruzzimo@naver.com>
2022-08-27 12:30:37 +0900

    🚧 product 도메인 클래스들 생성

---
+++ practicejpa/src/test/java/com/practicejpa/product/ProductControllerTest.java
@@ -0,0 +1,13 @@
+package com.practicejpa.product;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class ProductControllerTest {
+
+	@Test
+	void anything() {
+		fail();
+	}
+}
